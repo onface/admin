11009b0a9fc6f2522201a9fed93c33dcfd8e62c8
nimojs <nimo.jser@gmail.com>
2016-08-04 14:33:53 +0800

    增加初版 time 模块

---
+++ m/time/README.md
@@ -0,0 +1,38 @@
+# time
+
+## 日期
+
+<div id="demo">demo</div>
+
+````html
+<span
+	data-time-name="date"
+ ></span>
+````
+
+### 编辑状态
+
+````html
+<span
+	data-time-name="date"
+	data-time-value="2016-12-12"
+ >1992-12-19</span>
+````
+
+### 限制最小时间
+
+````html
+<span
+	data-time-name="date"
+	data-time-min="2016-12-10"
+ >2016-12-19</span>
+````
+
+### 限制最大时间
+
+````html
+<span
+	data-time-name="date"
+	data-time-max="2016-12-28"
+ >2016-12-19</span>
+````
---
+++ m/time/index.js
@@ -0,0 +1,99 @@
+import $ from 'jquery';
+import Calendar from 'rc-calendar';
+import 'rc-calendar/assets/index.css';
+import DatePicker from 'rc-calendar/lib/Picker';
+import MonthCalendar from 'rc-calendar/lib/MonthCalendar';
+import CalendarLocale from 'rc-calendar/lib/locale/zh_CN';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GregorianCalendar from "gregorian-calendar"
+import DateTimeFormat from 'gregorian-calendar-format';
+import zhCN from 'gregorian-calendar/lib/locale/zh_CN';
+const dateFormatter = new DateTimeFormat('yyyy-MM-dd');
+const formatter = new DateTimeFormat('yyyy-MM-dd HH:mm:ss');
+class Time extends React.Component {
+    static defaultProps = {
+        timeHasDate: true
+    }
+    state = {
+        value: this.props.value
+    }
+    change (data) {
+        let value = dateFormatter.format(data)
+        this.setState({
+            value: value
+        })
+    }
+    disabledDate (current) {
+        if (this.props.timeMin) {
+            if (current.time < new Date(this.props.timeMin).getTime()) {
+                return true
+            }
+        }
+        if (this.props.timeMax) {
+            if (current.time > new Date(this.props.timeMax).getTime()) {
+                return true
+            }
+        }
+        return false
+    }
+    render () {
+        let self = this
+        let calendar
+        // data-time-hasTime="true"
+        if (this.props.timeHasDate) {
+            // data-time-hasDate="ture"
+            // data-time-hasTime="true"
+            if (this.props.timeHasTime) {
+
+            }
+            // data-time-hasDate="ture"
+            // data-time-hasTime="false"
+            else {
+                let currentDate = new GregorianCalendar(zhCN)
+                if (this.state.value) {
+                    currentDate.setTime(new Date(this.state.value))
+                }
+                else {
+                    currentDate = false
+                }
+
+                calendar = (<Calendar
+                    formatter={dateFormatter}
+                    value={currentDate}
+                    disabledDate={self.disabledDate.bind(this)} 
+                    onChange={this.change.bind(this)}
+                     />)
+            }
+        }
+        // data-time-hasDate="false"
+        // data-time-hasTime="true"
+        else {
+
+        }
+        return (
+            <DatePicker
+                calendar={calendar}
+             >
+            {
+               function () {
+                   return (
+                       <input type="text" name={self.props.timeName} readOnly={true} value={self.state.value} />
+                   )
+               }
+           }
+            </DatePicker>
+        )
+    }
+}
+mo.time = function ($target) {
+    $target.each(function () {
+        let $this = $(this)
+        $this.addClass('mo-time')
+        let data = $this.data()
+        ReactDOM.render(<Time {...data} value={$this.html().replace(/\s/g,'')} />, this)
+    })
+}
+$(function () {
+    mo.time($("[data-time-name]"))
+})
