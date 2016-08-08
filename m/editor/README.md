92ca02f9cf3b18f5e617e4fed1266d8dc149233e
nimojs <nimo.jser@gmail.com>
2016-08-08 16:06:39 +0800

    simditor

---
+++ m/editor/README.md
@@ -0,0 +1,11 @@
+# editor
+
+````html
+<textarea 
+	name="some"
+	data-editor-name="content"
+	data-editor-upload-url="/m/json/ok-upload.json" 
+	>
+	<p>AAAA <strong>BBB</strong></p>
+</textarea>
+````
---
+++ m/editor/index.js
@@ -0,0 +1,67 @@
+import $ from "jquery"
+import './simditor.css'	
+window.Simditor_Upload_AJAX_filter = function (res) {
+	if (res.status === 'success') {
+	    res.success = true
+	    res.file_path = res.data.src
+	}
+	else {
+	    res.success = false
+	}
+	return res
+}
+
+$(function () {
+	$('[data-editor-name]').each(function () {
+		let $this = $(this)
+		let data = $this.data()
+		$this.addClass('mo-editor')
+		let editor = new Simditor({
+		textarea: this,
+		toolbar: [
+		  'title'
+		  ,
+		  'bold'
+		  ,
+		  'italic'
+		  ,
+		  'underline'
+		  ,
+		  'strikethrough'
+		  ,
+		  'fontScale'
+		  ,
+		  'color'
+		  ,
+		  'ol'
+		  ,
+		  'ul'
+		  ,
+		  'blockquote'
+		  ,
+		  'code'
+		  ,
+		  'table'
+		  ,
+		  'link'
+		  ,
+		  'image'
+		  ,
+		  'hr'
+		  ,
+		  'indent'
+		  ,
+		  'outdent'
+		  ,
+		  'alignment'
+		  ,
+		  'html'
+		],
+		pasteImageL: false,
+		upload: {
+		    // 可通过修改这里改变 图片ajax上传地址
+		    url: data.editorUploadUrl
+		}
+		});
+	})
+})
