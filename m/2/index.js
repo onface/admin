d7d1b5ad662f76d71087227ab35fc3cb271b515d
Nimo <nimo.jser@gmail.com>
2017-06-07 18:05:42 +0800

    sync side

--- m/2/index.js
+++ m/2/index.js
@@ -6,4 +6,14 @@ $(function () {
         $item.toggleClass('mo-2-side-item--on')
     })
     $('.mo-2-sub-status').addClass('mo-2-sub-status--done').fadeOut(2000)
+    function syncCnt () {
+        var minCntHeight = $(window).height() - $('.mo-2-head').height() - 10
+        var $cntHeight = $('.mo-2-cnt').height()
+        if ($cntHeight < minCntHeight) {
+            $('.mo-2-cnt').height(minCntHeight)
+        }
+        $('.mo-2-side').height($('.mo-2-cnt').height())
+    }
+    syncCnt()
+    $(window).on('resize', syncCnt)
 })
