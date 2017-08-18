d640be8ed73cb0df8d1352b98b6b3bd6ebb6aaf4
Grifree <1149290648@qq.com>
2017-08-18 17:14:27 +0800

    login5 验证码

--- m/login5/index.js
+++ m/login5/index.js
@@ -1,5 +1,21 @@
 let $ = require('jquery')
 $(function(){
-	
+	function createTimestamp (ele) {
+		let $this = $(this)
+		let src = $this.data().loginVerifySrc
+		if (/\?/.test(src)) {
+			src = src + '&'
+		}
+		else {
+			src = src + '?'
+		}
+		src = src + 'timestamp=' + new Date().getTime()
+		$this.attr('src', src)
+	}
+	let $code = $('.mo-login5-form-item-verifyimage')
+	$code.each(function () {
+		let $this = $(this)
+		createTimestamp.bind(this)()
+	}).on('click',createTimestamp)
 
 })
